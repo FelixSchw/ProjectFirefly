28f5324e519250d15f1cd6fa81d9db6c6055f62d
leopoldspenner <l.spenner@hotmail.de>
2017-06-12 12:48:04 +0200

    - added two deep learning network topologies (one deeper, one wider)

--- NeuralRegressionNetwork.py
+++ NeuralRegressionNetwork.py
@@ -171,7 +171,7 @@ def baseline_model():
 # fix random seed for reproducibility
 seed = 7
 
-# evaluate model with standardized dataset
+# evaluate model with original dataset
 estimator = KerasRegressor(build_fn=baseline_model, nb_epoch=100, batch_size=5, verbose=0)
 kfold = KFold(n_splits=10, random_state=seed)
 results = cross_val_score(estimator,X ,Y , cv=kfold)
@@ -186,3 +186,42 @@ pipeline = Pipeline(estimators)
 kfoldS = KFold(n_splits=10, random_state=seed)
 resultsS = cross_val_score(pipeline, X, Y, cv=kfoldS)
 print("Standardized: %.2f (%.2f) MSE" % (resultsS.mean(), results.std()))
+
+# define the model for a deeper network
+def larger_model():
+    # create model
+    model = Sequential()
+    model.add(Dense(13, input_dim=13, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(6, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(1, kernel_initializer='normal'))
+    # Compile model
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    return model
+
+np.random.seed(seed)
+estimators = []
+estimators.append(('standardize', StandardScaler()))
+estimators.append(('mlp', KerasRegressor(build_fn=larger_model, epochs=50, batch_size=5, verbose=0)))
+pipeline = Pipeline(estimators)
+kfold = KFold(n_splits=10, random_state=seed)
+results = cross_val_score(pipeline, X, Y, cv=kfold)
+print("Larger: %.2f (%.2f) MSE" % (results.mean(), results.std()))
+
+# define wider model
+def wider_model():
+    # create model
+    model = Sequential()
+    model.add(Dense(20, input_dim=13, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(1, kernel_initializer='normal'))
+    # Compile model
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    return model
+
+np.random.seed(seed)
+estimators = []
+estimators.append(('standardize', StandardScaler()))
+estimators.append(('mlp', KerasRegressor(build_fn=wider_model, epochs=100, batch_size=5, verbose=0)))
+pipeline = Pipeline(estimators)
+kfold = KFold(n_splits=10, random_state=seed)
+results = cross_val_score(pipeline, X, Y, cv=kfold)
+print("Wider: %.2f (%.2f) MSE" % (results.mean(), results.std()))
