3c1bbb938b2e613ac4859d931401403bb36b09e2
leopoldspenner <l.spenner@hotmail.de>
2017-06-15 07:39:52 +0200

    - changed python file structure

---
+++ snapZero.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+#author: Schweikardt & Spenner
+#version: 1.0 May 2017
+
+import pandas as pd
+import numpy as np
+import os
+
+
+###Only apply if default directories are not working###
+
+###Change working directory###
+from sklearn.preprocessing import StandardScaler
+
+felixOrLeo = "l"
+
+if (felixOrLeo == "f"):
+    pathData = "C:\\Users\\Felix Schweikardt\\Dropbox\\Seminararbeit FZI - Softsensor\\Datensätze"
+    pathInterface = "C:\\Users\\Felix Schweikardt\\Dropbox\\Seminararbeit FZI - Softsensor\\Interface"
+else:
+    pathData = "/Users/leopoldspenner/Dropbox/Seminararbeit FZI - Softsensor/Datensätze"
+    pathInterface = "/Users/leopoldspenner/Dropbox/Seminararbeit FZI - Softsensor/Interface"
+
+f = "C:\\Users\\Felix Schweikardt\\Dropbox\\Seminararbeit FZI - Softsensor\\Datensätze"
+fInterface = "C:\\Users\\Felix Schweikardt\\Dropbox\\Seminararbeit FZI - Softsensor\\Interface"
+l = "/Users/leopoldspenner/Dropbox/Seminararbeit FZI - Softsensor/Datensätze"
+lInterface = "/Users/leopoldspenner/Dropbox/Seminararbeit FZI - Softsensor/Interface"
+
+### extract y values
+os.chdir(pathData)
+cwd = os.getcwd()
+dateparse = lambda x: pd.datetime.strptime(x, '%Y-%m-%d %H:%M:%S')
+trainingDataTargets = pd.read_csv('Targets.txt', parse_dates=['Time'], date_parser=dateparse)
+trainingDataTargets = trainingDataTargets.set_index('Time')
+
+### extract 'TrainingDataAlloc' from previous module 'Preprocessing'
+
+
+
