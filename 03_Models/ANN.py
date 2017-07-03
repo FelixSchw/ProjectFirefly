88ffdbb940eb03f6c239af101ca3832e7d21b3a3
leopoldspenner <l.spenner@hotmail.de>
2017-07-03 18:27:50 +0200

    - implemented standardized and normalized NNs

--- 03_Models/ANN.py
+++ 03_Models/ANN.py
@@ -15,6 +15,7 @@ from sklearn.model_selection import KFold
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import make_scorer, mean_squared_error
 from sklearn import preprocessing
+from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.wrappers.scikit_learn import KerasRegressor
@@ -50,28 +51,40 @@ filenames.append("SnapZero.csv")
 for i in filenames:
 
 
+    ##### Set seed and test_size
+    seed = 1
+    test_size = 0.2
+
+
     ##### Read file with NN data, set index, and calculate # of predictors
     dataForRegression = pd.read_csv(i, parse_dates=['Time'], date_parser=dateparse)
     dataForRegression = dataForRegression.set_index('Time')
     numberOfPredictors = (len(dataForRegression.columns) - 1)
 
 
-    ##### Split NN data into input (X) and output(Y) data
+    ##### Split NN data into test and training data
     dataForRegression_X = dataForRegression.iloc[:,0:numberOfPredictors]
     dataForRegression_Y = dataForRegression.iloc[:,numberOfPredictors]
-    X = dataForRegression_X.values
-    Y = dataForRegression_Y.values
-
-
-    ##### Set seed and test_size
-    seed = 1
-    test_size = 0.2
-
-
-    ##### Split dataForRegression in subsets of test & training data with parameters seed & test-size
+    X = dataForRegression_X.values ### only for k-fold cross validation
+    Y = dataForRegression_Y.values ### only for k-fold cross validation
     dFR_X_train, dFR_X_test, dFR_Y_train, dFR_Y_test = cross_validation.train_test_split(dataForRegression_X, dataForRegression_Y, test_size=test_size, random_state=seed)
     X_train, X_test, Y_train, Y_test = dFR_X_train.values, dFR_X_test.values, dFR_Y_train.values, dFR_Y_test.values
-    print("Now starting: Training with X_train.shape=" + str(X_train.shape) + " and predicting with X_test.shape=" + str(X_test.shape))
+
+
+    ##### Rescale data (often referred to as normalization) http://machinelearningmastery.com/prepare-data-machine-learning-python-scikit-learn/
+    minmaxscaler = MinMaxScaler(feature_range=(0, 1))
+    rescaledDataForRegression = minmaxscaler.fit_transform(dataForRegression)
+    dataForRegression_X_minmaxscaled = rescaledDataForRegression[:,0:numberOfPredictors]
+    dataForRegression_Y_minmaxscaled = rescaledDataForRegression[:,numberOfPredictors]
+    X_train_minmaxscaled, X_test_minmaxscaled, Y_train_minmaxscaled, Y_test_minmaxscaled = cross_validation.train_test_split(dataForRegression_X_minmaxscaled, dataForRegression_Y_minmaxscaled, test_size=test_size, random_state=seed)
+
+
+    ##### Standardize data (http://machinelearningmastery.com/prepare-data-machine-learning-python-scikit-learn/)
+    standardscaler = StandardScaler()
+    standardizedDataForRegression = standardscaler.fit_transform(dataForRegression)
+    dataForRegression_X_standardized = standardizedDataForRegression[:,0:numberOfPredictors]
+    dataForRegression_Y_standardized = standardizedDataForRegression[:,numberOfPredictors]
+    X_train_standardized, X_test_standardized, Y_train_standardized, Y_test_standardized = cross_validation.train_test_split(dataForRegression_X_standardized, dataForRegression_Y_standardized, test_size=test_size, random_state=seed)
 
 
     ##### Define base model
@@ -86,17 +99,36 @@ for i in filenames:
 
 
     ##### Fit baseline model on training data and make predictions using test data
-    estimator = KerasRegressor(build_fn=baseline_model, nb_epoch=200, batch_size=5, verbose=2)
-    estimator.fit(X_train, Y_train, batch_size=5, epochs=200, verbose=2)
+    estimator = KerasRegressor(build_fn=baseline_model, nb_epoch=400, batch_size=5, verbose=2)
+    estimator.fit(X_train, Y_train, batch_size=5, epochs=400, verbose=2)
     predictions = estimator.predict(X_test)
     current_model = "ANN" + i[:-4] + "Results.csv"
     print(current_model + " has RMSE of: " + str(errorFunction(predictions, Y_test)))
 
 
+    ##### Fit baseline model on minmaxscaled training data, make prediction on test data and inverse transform it
+    estimator_minmaxscaled = KerasRegressor(build_fn=baseline_model, nb_epoch=180, batch_size=5, verbose=2)
+    estimator_minmaxscaled.fit(X_train_minmaxscaled, Y_train_minmaxscaled, batch_size=5, epochs=180, verbose=2)
+    predictions_minmaxscaled = estimator_minmaxscaled.predict(X_test_minmaxscaled)[:, None]
+    predictions_minmaxscaled_matrix = np.hstack((np.ones((len(X_test), numberOfPredictors)), predictions_minmaxscaled))
+    predictions_minmaxinversed = minmaxscaler.inverse_transform(predictions_minmaxscaled_matrix)[:,numberOfPredictors]
+    print(current_model + " (minmaxscaled) has RMSE of: " + str(errorFunction(predictions_minmaxinversed, Y_test)))
+
+
+    ##### Fit baseline model on minmaxscaled training data, make prediction on test data and inverse transform it
+    estimator_standardized = KerasRegressor(build_fn=baseline_model, nb_epoch=400, batch_size=5, verbose=2)
+    estimator_standardized.fit(X_train_standardized, Y_train_standardized, batch_size=5, epochs=400, verbose=2)
+    predictions_standardized = estimator_standardized.predict(X_test_standardized)[:, None]
+    predictions_standardized_matrix = np.hstack((np.ones((len(X_test), numberOfPredictors)), predictions_standardized))
+    predictions_standardizedinversed = standardscaler.inverse_transform(predictions_standardized_matrix)[:,numberOfPredictors]
+    print(current_model + " (standardized) has RMSE of: " + str(errorFunction(predictions_standardizedinversed, Y_test)))
+
+
+
     ##### Save dataframe with results
     to_save = pd.DataFrame(
         {'Time': dFR_X_test.index,
-         'Predictions': predictions,
+         'Predictions': predictions_standardizedinversed,
          'Feinheit': Y_test})
     to_save = to_save.set_index('Time')
     to_save.to_csv(current_model)
